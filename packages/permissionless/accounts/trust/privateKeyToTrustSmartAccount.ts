57332c0fb3df3f28035f7f15647c89732b72c8f2
pavlovdog <pavlovdog.moscow@gmail.com>
2024-05-16 12:48:44 +0400

    fix review

--- packages/permissionless/accounts/trust/privateKeyToTrustSmartAccount.ts
+++ packages/permissionless/accounts/trust/privateKeyToTrustSmartAccount.ts
@@ -2,35 +2,35 @@ import { type Chain, type Client, type Hex, type Transport } from "viem"
 import { privateKeyToAccount } from "viem/accounts"
 import type { ENTRYPOINT_ADDRESS_V06_TYPE, Prettify } from "../../types"
 import {
-    type LightSmartAccount,
-    type SignerToLightSmartAccountParameters,
-    signerToLightSmartAccount
-} from "./signerToLightSmartAccount"
+    type SignerToTrustSmartAccountParameters,
+    type TrustSmartAccount,
+    signerToTrustSmartAccount
+} from "./signerToTrustSmartAccount"
 
-export type PrivateKeyToLightSmartAccountParameters<
+export type PrivateKeyToTrustSmartAccountParameters<
     entryPoint extends ENTRYPOINT_ADDRESS_V06_TYPE
 > = Prettify<
     {
         privateKey: Hex
-    } & Omit<SignerToLightSmartAccountParameters<entryPoint>, "signer">
+    } & Omit<SignerToTrustSmartAccountParameters<entryPoint>, "signer">
 >
 
 /**
- * @description Creates an Light Account from a private key.
+ * @description Creates an Trust Account from a private key.
  *
- * @returns A Private Key Light Account.
+ * @returns A Private Key Trust Account.
  */
-export async function privateKeyToLightSmartAccount<
+export async function privateKeyToTrustSmartAccount<
     entryPoint extends ENTRYPOINT_ADDRESS_V06_TYPE,
     TTransport extends Transport = Transport,
     TChain extends Chain | undefined = Chain | undefined
 >(
     client: Client<TTransport, TChain, undefined>,
-    { privateKey, ...rest }: PrivateKeyToLightSmartAccountParameters<entryPoint>
-): Promise<LightSmartAccount<entryPoint, TTransport, TChain>> {
+    { privateKey, ...rest }: PrivateKeyToTrustSmartAccountParameters<entryPoint>
+): Promise<TrustSmartAccount<entryPoint, TTransport, TChain>> {
     const privateKeyAccount = privateKeyToAccount(privateKey)
 
-    return signerToLightSmartAccount(client, {
+    return signerToTrustSmartAccount(client, {
         signer: privateKeyAccount,
         ...rest
     })
